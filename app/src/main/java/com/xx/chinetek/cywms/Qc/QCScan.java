c832bd7caf680683876cdd34948bdc26cee08ac9
徐鑫 <ghost_511@sina.com>
2017-06-28 16:37:35 +0800

    QC

--- app/src/main/java/com/xx/chinetek/cywms/Qc/QCScan.java
+++ app/src/main/java/com/xx/chinetek/cywms/Qc/QCScan.java
@@ -5,19 +5,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
-import com.xx.chinetek.adapter.Receiption.ReceiptScanDetailAdapter;
 import com.xx.chinetek.base.BaseActivity;
 import com.xx.chinetek.base.BaseApplication;
 import com.xx.chinetek.base.ToolBarTitle;
 import com.xx.chinetek.cywms.R;
-import com.xx.chinetek.model.Receiption.ReceiptDetail_Model;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @ContentView(R.layout.activity_qcscan)
@@ -33,9 +28,9 @@ public class QCScan extends BaseActivity {
         BaseApplication.context = context;
         BaseApplication.toolBarTitle = new ToolBarTitle(getString(R.string.QC_subtitle), true);
         x.view().inject(this);
-        List<ReceiptDetail_Model> receiptDetailModels=getData();
-        ReceiptScanDetailAdapter receiptScanDetailAdapter=new ReceiptScanDetailAdapter(context,receiptDetailModels);
-        lsvQCScan.setAdapter(receiptScanDetailAdapter);
+        //List<ReceiptDetail_Model> receiptDetailModels=getData();
+//        ReceiptScanDetailAdapter receiptScanDetailAdapter=new ReceiptScanDetailAdapter(context,receiptDetailModels);
+//        lsvQCScan.setAdapter(receiptScanDetailAdapter);
     }
 
     @Override
@@ -53,15 +48,15 @@ public class QCScan extends BaseActivity {
     }
 
 
-    List<ReceiptDetail_Model> getData(){
-        List<ReceiptDetail_Model> receiptDetailModels=new ArrayList<>();
-        for(int i=0;i<10;i++){
-            ReceiptDetail_Model receiptDetailModel=new ReceiptDetail_Model();
-            receiptDetailModel.setMaterialNo("条码"+i);
-            receiptDetailModel.setMaterialDesc("物料描述"+i);
-            receiptDetailModel.setScanQty(1f);
-            receiptDetailModels.add(receiptDetailModel);
-        }
-        return receiptDetailModels;
-    }
+//    List<ReceiptDetail_Model> getData(){
+//        List<ReceiptDetail_Model> receiptDetailModels=new ArrayList<>();
+//        for(int i=0;i<10;i++){
+//            ReceiptDetail_Model receiptDetailModel=new ReceiptDetail_Model();
+//            receiptDetailModel.setMaterialNo("条码"+i);
+//            receiptDetailModel.setMaterialDesc("物料描述"+i);
+//            receiptDetailModel.setScanQty(1f);
+//            receiptDetailModels.add(receiptDetailModel);
+//        }
+//        return receiptDetailModels;
+//    }
 }
